a3def4062a135f9d9d87e18477598ed4d048787e
em1nm <em.martirosyan@gmail.com>
2023-05-09 12:21:33 -0700

    updated html and css

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,68 @@
-//html elements
-var saveButton = $('.saveBtn');
-var timeBlockContainer = $('#time-blocks');
+var saveButton = $(".btn");
+var row = $(".row");
+var now = dayjs();
 
-//html element divs
-var hourNineDiv = $('#hour-9');
-var hourTenDiv = $('#hour-10');
-var hourElevenDiv = $('#hour-11');
-var hourTwelveDiv = $('#hour-12');
-var hourThirteenDiv = $('#hour-13');
-var hourFourteenDiv = $('#hour-14');
-var hourFifteenDiv = $('#hour-15');
-var hourSixteenDiv = $('#hour-16');
-var hourSeventeenDiv = $('#hour-17');
+// event listener for each save button. getting the id for each parent by using $(this). getting the input value by getting the previous childs input value and then saving to local storage. the key is the id and the value is the input text.
+saveButton.on("click", function () {
+  var timeStorage = $(this).parent().attr("id");
+  var input = $(this).prev().val();
+  localStorage.setItem(timeStorage, input);
+});
 
-// input elements in an array to loop through
-var divElements = [
-  {htmlEl: hourNineDiv, hour: 9},
-  {htmlEl: hourTenDiv, hour: 10},
-  {htmlEl: hourElevenDiv, hour: 11},
-  {htmlEl: hourTwelveDiv, hour: 12},
-  {htmlEl: hourThirteenDiv, hour: 13},
-  {htmlEl: hourFourteenDiv, hour: 14},
-  {htmlEl: hourFifteenDiv, hour: 15},
-  {htmlEl: hourSixteenDiv, hour: 16},
-  {htmlEl: hourSeventeenDiv, hour: 17}
-  //returns a number
-]
+// this function loops through each row while getting the id of each row. i'm getting the current hour with day js and then compare the id to the current hour. were then setting the input's class depending the hour of the day.
 
-//variables
-var timeNow = dayjs();
-//returns string
-var currentHour = timeNow.format('H'); 
-console.log(currentHour);
-var dailySchedule = [];
+function getTime() {
+  row.each(function () {
+    var timeHour = $(this).attr("id").split("-")[1];
+    var currentHour = now.hour();
+    var inp = $(this).children().eq(1);
+
+    if (timeHour < currentHour) {
+      inp.addClass("past");
+    } else if (timeHour > currentHour) {
+      inp.addClass("future");
+    } else {
+      inp.addClass("present");
+    }
+  });
+}
+
+// this function updates the time with the set interval function. i'm updating the time by 1 second.
+
+function updateTime() {
+  setInterval(function () {
+    var currentDay = $("#currentDay");
+    var currentDate = now.format("MM/DD/YYYY");
+    var currentTime = now.format("h:mm A");
+    currentDay.text(`Current time: ${currentDate} ${currentTime}`);
+  }, 1000);
+}
+
+// this function updates the inputs from local storage. were setting the index at 9 becasue the ids go from 9 to 15 (3pm).
+
+function updatedInputs() {
+  var index = 9;
+
+  row.each(function () {
+    var storage = "hour-" + index;
+    var lastInputs = localStorage.getItem(storage);
+
+    if ($(this).attr("id") === storage) {
+      if (lastInputs !== null) {
+        $(this).children().eq(1).text(lastInputs);
+        console.log(lastInputs);
+      }
+    }
+
+    index++;
+  });
+}
+
+// this function calls everything so it will run in order.
+function run() {
+  updateTime();
+  getTime();
+  updatedInputs();
+}
+
+run();
